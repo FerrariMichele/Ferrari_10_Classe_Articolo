4e5e86556ca3fff3da5129ab48e94fcc57c7c9f0
Ferrari.20190 <Ferrari.20190@E2-9-PC07>
2023-11-21 13:19:35 +0100

    ArticoloAlimentareFresco

--- Ferrari_10_Classe_Articolo/ArticoloAlimentareFresco.cs
+++ Ferrari_10_Classe_Articolo/ArticoloAlimentareFresco.cs
@@ -8,10 +8,15 @@ namespace Ferrari_10_Classe_Articolo
 {
     public class ArticoloAlimentareFresco : ArticoloAlimentare
     {
-
+        private int _daysTillExp;
+        public int DaysTillExp
+        {
+            get { return _daysTillExp; }
+            set { _daysTillExp = value; }
+        }
         public ArticoloAlimentareFresco (string prodID, string report, double unitPrice, string expYear, int daysTillExp) : base(prodID, report, unitPrice, expYear)
         {
-            daysTillExp = 
+            DaysTillExp = daysTillExp;
         }
         public override double Sconta(bool isMember)
         {
@@ -22,7 +27,7 @@ namespace Ferrari_10_Classe_Articolo
             }
             if (ExpYear == DateTime.Now.Year.ToString())
             {
-                finalPrice = finalPrice / 100 * 80;
+                finalPrice = finalPrice / 100 * (100 - (5 - DaysTillExp + 1) * 2);
             }
             return finalPrice;
         }
