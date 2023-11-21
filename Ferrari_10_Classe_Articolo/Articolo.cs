7ea8bd2d6c717cad4d19f47ee9dad2785f24be45
Ferrari.20190 <Ferrari.20190@E2-9-PC07>
2023-11-21 12:27:55 +0100

    Aggiungere i file di progetto.

---
+++ Ferrari_10_Classe_Articolo/Articolo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari_10_Classe_Articolo
+{
+    internal class Articolo
+    {
+    }
+}
