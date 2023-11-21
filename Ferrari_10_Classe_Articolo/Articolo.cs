9cc06e4d37ce448708f8f9a79b0232a4c80c859e
Ferrari.20190 <Ferrari.20190@E2-9-PC07>
2023-11-21 12:39:45 +0100

    Classe Articolo

--- Ferrari_10_Classe_Articolo/Articolo.cs
+++ Ferrari_10_Classe_Articolo/Articolo.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Ferrari_10_Classe_Articolo
 {
-    internal class Articolo
+    public class Articolo
     {
+        private string _prodID;
+        private string _report;
+        private double _unitPrice;
+        public string ProdID
+        {
+            get { return _prodID; }
+            set { _prodID = value; }
+        }
+        public string Report
+        {
+            get { return _report; }
+            set { _report = value; }
+        }
+        public double UnitPrice
+        { 
+            get { return _unitPrice; }
+            set { _unitPrice = value; }
+        }
+        public Articolo(string prodID, string report, double unitPrice)
+        {
+            ProdID = prodID;
+            Report = report;
+            UnitPrice = unitPrice;
+        }
+        public virtual double Sconta(bool isMember)
+        {
+            if (isMember)
+                return UnitPrice / 100 * 95;
+            else
+                return UnitPrice;
+        }
     }
 }
